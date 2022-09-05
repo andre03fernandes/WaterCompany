cf6aa822fadf5804b2a024942adc8a5c30b4718a
andre03fernandes <andre2411fernandes@gmail.com>
2022-09-05 20:39:12 +0100

    Layout Updated

--- WaterCompany/wwwroot/js/site.js
+++ WaterCompany/wwwroot/js/site.js
@@ -1,4 +1,60 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let time = new Date();
+let year = time.getFullYear();
+document.getElementById("rights").innerHTML = "© " + year + " André Fernandes. All rights reserved. Enjoy it!" + '<br><br>' +
+    " CINEL | UFCD 5417 - Programação para a WEB - servidor (server-side) | 25853";
 
-// Write your JavaScript code.
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Select The Elements
+var toggle_btn;
+var big_wrapper;
+var hamburger_menu;
+
+function declare() {
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
+    hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation() {
+    // Clone the wrapper
+    dark = !dark;
+    let clone = big_wrapper.cloneNode(true);
+    if (dark) {
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    } else {
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    clone.classList.add("copy");
+    main.appendChild(clone);
+
+    document.body.classList.add("stop-scrolling");
+
+    clone.addEventListener("animationend", () => {
+        document.body.classList.remove("stop-scrolling");
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+        // Reset Variables
+        declare();
+        events();
+    });
+}
+
+function events() {
+    toggle_btn.addEventListener("click", toggleAnimation);
+    hamburger_menu.addEventListener("click", () => {
+        big_wrapper.classList.toggle("active");
+    });
+}
+
+events();
